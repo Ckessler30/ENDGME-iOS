eebdcaf5a9ecc86c30f8956869298a0c063e50e3
Chase Kessler <83434134+Ckessler30@users.noreply.github.com>
2024-12-28 14:01:39 -0500

    Initial Commit

---
+++ ENDGME/ENDGMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ENDGMEApp.swift
+//  ENDGME
+//
+//  Created by Chase Kessler on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ENDGMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
